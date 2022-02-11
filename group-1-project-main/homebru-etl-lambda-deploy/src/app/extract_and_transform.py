e071797de0958eef8c9dedc5f481093d0267c360
StarhVR <94742344+StarhVR@users.noreply.github.com>
2022-02-11 17:50:34 +0000

    New Main Branch
    Rearranged everything

---
+++ group-1-project-main/homebru-etl-lambda-deploy/src/app/extract_and_transform.py
@@ -0,0 +1,82 @@
+import uuid
+import pandas as pd
+from hashlib import sha256
+
+def transform(filename):
+    try:
+        # from here onwards, until the next comment line is meant to be implemented from the extract.py file, but this execution/connection cannot be done yet
+        df = pd.read_csv(filename, names=[
+            'order_time',  
+            'branch_name',
+            'customer_name',
+            'order_products',
+            'total_price',
+            'payment_type',
+            'card_number'])
+        df = df.drop(columns=['branch_name','customer_name','card_number'])
+        df = df.dropna()
+        # end of previous comment
+        # create uuid for each transaction
+        df['order_id'] = [uuid.uuid4() for _ in range (len(df.index))]
+        column_names = [
+            'order_id',
+            'order_time', 
+            'order_products',
+            'total_price',
+            'payment_type']
+        df = df.reindex(columns=column_names)
+        # explode data to create orders table
+        orders = pd.DataFrame(df.order_products.str.split(", ").tolist(), index = df.order_id).stack()
+        orders = orders.reset_index([0,'order_id'])
+        orders.columns = ['order_id','product']
+        orders = pd.merge(orders, df[['order_time','total_price','payment_type','order_id']], on='order_id', how='left')
+        # add product_id
+        orders['product_id'] = orders['product'].apply(lambda x: str(int(sha256(x.encode('utf-8')).hexdigest(), 16))[:10])
+        # add product_name and product_price
+        orders[['product_name', 'product_price']] = orders['product'].str.rsplit(' - ', 1, expand=True)
+        
+        # do these parts before you remove stuff from orders
+        order_products = orders.groupby(['order_id','product_id','product_name','product_price'])
+
+        # remove unnecessary columns and add quantity after calculating the duplicates
+        order_products = order_products.size().reset_index(name='quantity')
+        column_names = ['order_id',
+                        'product_id',
+                        'quantity']
+        order_products_data = order_products.reindex(columns=column_names)
+
+                
+        # sort the orders table to be converted for the products table
+        products = orders.groupby(['product_id','product_name','product_price'])
+        products = products.size().reset_index()
+        column_names = ['product_id',
+                        'product_name',
+                        'product_price']
+        products_data = products.reindex(columns=column_names)
+
+        orders = orders.drop(columns=['product','product_id','product_name','product_price'])
+        # re-index orders table
+        column_names = ['order_id',
+                        'order_time',
+                        'payment_type',
+                        'total_price']
+        orders = orders.reindex(columns=column_names)
+        orders_data = orders.drop_duplicates()
+            
+        # sort the orders table to be converted for the order products table (but keep product_id and product_name temporarily for the quantity column)
+
+        # # print the transformed tables
+        # print("\n---------------------------ORDERS TABLE---------------------------")
+        # print(orders)
+        # print("\n---------------------------ORDER PRODUCTS TABLE---------------------------")
+        # print(order_products)
+        # print("\n---------------------------PRODUCTS TABLE---------------------------")
+        # print(products)
+    except Exception as error:
+        print("An error occurred: " + str(error))
+        
+    print(orders_data, order_products_data, products_data)
+    return orders_data, order_products_data, products_data
+
+# transform('chesterfield.csv')
+# print(orders, order_products, products)
---
+++ group-1-project-main/homebru-etl-lambda-deploy/src/app/homebru_etl_lambda.py
@@ -0,0 +1,51 @@
+import logging
+import boto3
+import os
+import pandas as pd
+import app.extract_and_transform as extract_and_transform
+import app.database as database
+
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.INFO)
+
+def lambda_handler(event, context):
+    LOGGER.info(event)
+
+    s3_event = event["Records"][0]["s3"]
+    bucket_name = s3_event["bucket"]["name"]
+    object_name = s3_event["object"]["key"]
+
+    file_path = (f"/tmp/{object_name}")
+
+    LOGGER.info(f"Triggered by file {object_name} in bucket {bucket_name}")
+
+    s3 = boto3.client('s3')
+    s3.download_file(bucket_name, object_name, file_path)
+
+    response = s3.list_objects(Bucket='homebru-cafe-data-bucket')
+    print([x["Key"] for x in response["Contents"]])
+    print(os.path.isfile('/tmp/chesterfield.csv'))
+    
+    # This part will be replaced with our ETL code to Transform our cafe data ready for RedShift
+    transformed_data = extract_and_transform.transform(f"/tmp/{object_name}")
+
+    # This is needed for credentials to the RedShift database
+    # creds = get_ssm_parameters_under_path("/team1/redshift")
+
+    # Try to get this working first and then try the rest
+    # database.persist_products(creds, transformed_data["products_data"])
+
+
+    # database.persist_order_products(creds, transformed_data["order_products_data"])
+    # database.persist_orders(creds, transformed_data["orders_data"])
+
+def get_ssm_parameters_under_path(path: str) -> dict:
+
+    ssm_client = boto3.client("ssm", region_name="eu-west-1")
+    response = ssm_client.get_parameters_by_path(
+        Path=path,
+        Recursive=True,
+        WithDecryption=True
+    )
+    formatted_response = {os.path.basename(x["Name"]):x["Value"] for x in response["Parameters"]}
+    return formatted_response
