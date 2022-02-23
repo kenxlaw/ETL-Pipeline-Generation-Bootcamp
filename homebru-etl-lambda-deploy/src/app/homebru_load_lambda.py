481c1c0a97e50b47b5d8e64886382ad5d20a93ca
StarhVR <94742344+StarhVR@users.noreply.github.com>
2022-02-23 15:45:23 +0000

    Add files via upload

--- homebru-etl-lambda-deploy/src/app/homebru_load_lambda.py
+++ homebru-etl-lambda-deploy/src/app/homebru_load_lambda.py
@@ -23,7 +23,7 @@ def lambda_handler(event, context):
     # s3 = boto3.client('s3')
 
     file_name = os.path.basename(object_name)
-    file_path = f"/tmp/{file_name}"
+    # file_path = f"/tmp/{file_name}"
 
     # s3.download_file(bucket_name, object_name, file_path)
 
@@ -34,29 +34,19 @@ def lambda_handler(event, context):
     creds = get_ssm_parameters_under_path("/team1/redshift")
 
     if get_data_type(object_name) == "products":
-        products_transformed_data = read_file(object_name)        
+        products_transformed_data = read_file(file_name)        
         database.insert_products(creds, products_transformed_data)
         print(f"The products from {file_name} have successfully been loaded into the RedShift team1_cafe.products table")
     elif get_data_type(object_name) == "baskets":
-        basket_transformed_data = read_file(object_name)
+        basket_transformed_data = read_file(file_name)
         database.insert_basket(creds, basket_transformed_data)
         print(f"The baskets from {file_name} have successfully been loaded into the RedShift team1_cafe.basket table")
     elif get_data_type(object_name) == "transactions":
-        transactions_transformed_data = read_file(object_name)
+        transactions_transformed_data = read_file(file_name)
         database.insert_transactions(creds, transactions_transformed_data) 
         print(f"The orders from {file_name} have successfully been loaded into the RedShift team1_cafe.transactions table")
     else:
         print(f"Invalid file type for file {object_name}")
-
-def get_data_type(object_name):
-    if "_products.csv" in object_name:
-        return "products"
-    elif "_baskets.csv" in object_name:
-        return "baskets"
-    elif "_transactions.csv" in object_name:
-        return "transactions"
-    else:
-        raise Exception("Unexpected data type")
 
 def get_data_type(object_name):
     if "_products.csv" in object_name:
