9b943e51f18db8eb69b847b6ddbbae560a09b216
StarhVR <94742344+StarhVR@users.noreply.github.com>
2022-02-23 11:36:58 +0000

    Add files via upload

--- homebru-etl-lambda-deploy/src/app/homebru_extract_transform_lambda.py
+++ homebru-etl-lambda-deploy/src/app/homebru_extract_transform_lambda.py
@@ -29,14 +29,12 @@ def lambda_handler(event, context):
 
     results = extract_and_transform.transform(file_path)
 
-    sqs = boto3.client('sqs')
+    sqs = boto3.resource('sqs')
 
     send_file(s3, sqs, results["products_data"], "products", file_name.rsplit('.', 1)[0] + "_products.csv")
-    LOGGER.info(event)
     send_file(s3, sqs, results["order_products_data"], "order_products", file_name.rsplit('.', 1)[0] + "_baskets.csv")
-    LOGGER.info(event)
     send_file(s3, sqs, results["orders_data"], "orders", file_name.rsplit('.', 1)[0] + "_transactions.csv")
-    LOGGER.info(event)
+
 
 def send_file(s3, sqs, data_set, data_type: str, bucket_key: str):
 
