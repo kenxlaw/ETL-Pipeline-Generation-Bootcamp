b4c6f5f761acd14a7688ef2455a47874776f2995
StarhVR <94742344+StarhVR@users.noreply.github.com>
2022-02-23 14:33:02 +0000

    Add files via upload

--- homebru-etl-lambda-deploy/src/app/homebru_load_lambda.py
+++ homebru-etl-lambda-deploy/src/app/homebru_load_lambda.py
@@ -13,9 +13,10 @@ LOGGER.setLevel(logging.INFO)
 def lambda_handler(event, context):
     LOGGER.info(event)
 
-    s3_event = event["Records"][0]["body"]
-    bucket_name = s3_event["bucket_name"]
-    object_name = s3_event["bucket_key"]
+    sqs_event = event["Records"][0]["body"]
+    sqs_dict = json.loads(sqs_event)
+    bucket_name = sqs_dict["bucket_name"]
+    object_name = sqs_dict["bucket_key"]
 
     LOGGER.info(f"Triggered by file {object_name} in bucket {bucket_name}")
 
@@ -26,21 +27,21 @@ def lambda_handler(event, context):
 
     s3.download_file(bucket_name, object_name, file_path)
 
-    stripped_file = file_path.rsplit('_products.csv', 1)[0]
-    stripped_file = file_path.rsplit('_baskets.csv', 1)[0]
-    stripped_file = file_path.rsplit('_transactions.csv', 1)[0]
+    # stripped_file = file_path.rsplit('_products.csv', 1)[0]
+    # stripped_file = file_path.rsplit('_baskets.csv', 1)[0]
+    # stripped_file = file_path.rsplit('_transactions.csv', 1)[0]
 
     creds = get_ssm_parameters_under_path("/team1/redshift")
 
-    if os.path.is_file(stripped_file + "_products") is True:
+    if "_products.csv" in object_name:
         products_transformed_data = read_products(file_path)        
         database.insert_products(creds, products_transformed_data)
         print(f"The products from {file_name} have successfully been loaded into the RedShift team1_cafe.products table")
-    if os.path.is_file(stripped_file + "_baskets.csv") is True:
+    elif "_baskets.csv" in object_name:
         basket_transformed_data = read_basket(file_path)
         database.insert_basket(creds, basket_transformed_data)
         print(f"The baskets from {file_name} have successfully been loaded into the RedShift team1_cafe.basket table")
-    elif os.path.is_file(stripped_file + "_transactions.csv") is True:
+    elif "_transactions.csv" in object_name:
         transactions_transformed_data = read_transactions(file_path)
         database.insert_transactions(creds, transactions_transformed_data) 
         print(f"The orders from {file_name} have successfully been loaded into the RedShift team1_cafe.transactions table")
