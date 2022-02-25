5100933fd09a28be6a8fe796664df6ad07bc0d16
StarhVR <94742344+StarhVR@users.noreply.github.com>
2022-02-25 04:46:05 +0000

    Add files via upload

--- homebru-etl-lambda-deploy/src/app/homebru_backup_lambda.py
+++ homebru-etl-lambda-deploy/src/app/homebru_backup_lambda.py
@@ -29,17 +29,3 @@ def lambda_handler(event, context):
         'statusCode': 3000,
         'body': json.dumps(f'A cafe data backup has been created.\n{file_name} has been been copied from {source_bucket_name} to {destination_bucket_name}')
     }
-
-    s3_event = event["Records"][0]["s3"]
-    bucket_name = s3_event["bucket"]["name"]
-    object_name = s3_event["object"]["key"]
-
-    LOGGER.info(f"Triggered by file {object_name} in bucket {bucket_name}")
-
-    s3 = boto3.client('s3')
-
-    file_name = os.path.basename(object_name)
-
-    s3.download_file(bucket_name, object_name, object_name)
-
-    print(f"{file_name} has successfully been downloaded to the Homebru Cafe Data Backup Bucket in the location {object_name}")
