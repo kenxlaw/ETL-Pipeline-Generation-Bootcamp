cf15662f5bfd7f492f4d13b5e693250154029c29
StarhVR <94742344+StarhVR@users.noreply.github.com>
2022-02-28 10:36:25 +0000

    Delete homebru-etl-lambda-deploy/homebru-etl-lambda-deploy directory

--- homebru-etl-lambda-deploy/homebru-etl-lambda-deploy/local_transform.py
+++
@@ -1,6 +0,0 @@
-import app.extract_and_transform as extract_and_transform
-
-results = extract_and_transform.transform('chesterfield.csv')
-
-print(type(results["products_data"]))
-print(results["products_data"])
